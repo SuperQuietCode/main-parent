6129d053b7add3941ec4db620acb5e0ebf4d995f
zhaoxin <Zhaobinjie0607@foxmail.com>
2020-11-13 15:42:16 +0800

    解决项目不能打包的问题

---
+++ main-common/src/main/java/com/example/common/services/ApiServices.java
@@ -0,0 +1,9 @@
+package com.example.common.services;
+
+/**
+ * @author ZhaoXin
+ * @date 2020/11/13 15:39
+ */
+public interface ApiServices {
+
+}
---
+++ main-common/src/main/java/com/example/common/services/impl/ApiServicesImpl.java
@@ -0,0 +1,9 @@
+package com.example.common.services.impl;
+
+/**
+ * @author ZhaoXin
+ * @date 2020/11/13 15:40
+ */
+public class ApiServicesImpl {
+
+}
